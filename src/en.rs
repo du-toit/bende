077f6190f38b944a1e9336cc1319fcee27c5de3c
Rickz75 <rickz75dev@gmail.com>
2022-04-22 13:11:03 +0200

    Implement Serializer for the KeyEncoder type.

--- src/en.rs
+++ src/en.rs
@@ -3,6 +3,7 @@
 use std::io::Error as IoError;
 use std::io::Write;
 
+use serde::ser::Impossible;
 use serde::ser::SerializeMap;
 use serde::ser::SerializeSeq;
 use serde::ser::SerializeStruct;
@@ -532,6 +533,194 @@ impl<'a, W> KeyEncoder<'a, W> {
     #[inline]
     fn new(en: &'a mut Encoder<W>) -> KeyEncoder<'a, W> {
         Self { en }
+    }
+}
+
+impl<'a, W: Write> Serializer for KeyEncoder<'a, W> {
+    type Ok = ();
+
+    type Error = Error;
+
+    type SerializeSeq = Impossible<(), Error>;
+
+    type SerializeTuple = Impossible<(), Error>;
+
+    type SerializeTupleStruct = Impossible<(), Error>;
+
+    type SerializeTupleVariant = Impossible<(), Error>;
+
+    type SerializeMap = Impossible<(), Error>;
+
+    type SerializeStruct = Impossible<(), Error>;
+
+    type SerializeStructVariant = Impossible<(), Error>;
+
+    fn serialize_bool(self, _: bool) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_i8(self, _: i8) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_i16(self, _: i16) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_i32(self, _: i32) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_i64(self, _: i64) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_u8(self, _: u8) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_u16(self, _: u16) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_u32(self, _: u32) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_u64(self, _: u64) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_f32(self, _: f32) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_f64(self, _: f64) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_char(self, _: char) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        self.en.serialize_str(v)
+    }
+
+    fn serialize_bytes(self, _: &[u8]) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_some<T: ?Sized>(self, _: &T) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_unit_struct(
+        self,
+        _: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _: &'static str,
+        _: u32,
+        _: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_newtype_struct<T: ?Sized>(
+        self,
+        _: &'static str,
+        _: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_newtype_variant<T: ?Sized>(
+        self,
+        _: &'static str,
+        _: u32,
+        _: &'static str,
+        _: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_seq(
+        self,
+        _: Option<usize>,
+    ) -> Result<Self::SerializeSeq, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_tuple(
+        self,
+        _: usize,
+    ) -> Result<Self::SerializeTuple, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _: &'static str,
+        _: usize,
+    ) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _: &'static str,
+        _: u32,
+        _: &'static str,
+        _: usize,
+    ) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_map(
+        self,
+        _: Option<usize>,
+    ) -> Result<Self::SerializeMap, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_struct(
+        self,
+        _: &'static str,
+        _: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
+        Err(Error::InvalidKeyType)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _: &'static str,
+        _: u32,
+        _: &'static str,
+        _: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        Err(Error::InvalidKeyType)
     }
 }
 
