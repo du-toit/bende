2e6ceb7c6f80bf0d52eaac611d17837fbca29cef
Rickz75 <rickz75dev@gmail.com>
2022-04-27 13:10:30 +0200

    Declare a type alias for a dictionary.

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,10 @@
+use std::collections::BTreeMap;
+
 /// A list of bencode values.
 pub type List = Vec<Value>;
+
+/// A **sorted** key-value map with keys that are UTF-8 valid strings.
+pub type Dict = BTreeMap<String, Value>;
 
 /// Represents any valid data type that can be encoded/decoded to and from bencode.
 #[derive(Debug, Clone, PartialEq, Eq)]
