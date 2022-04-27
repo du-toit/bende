1fe9f8a094308bbcec18565503d804af67094474
Rickz75 <rickz75dev@gmail.com>
2022-04-27 13:01:44 +0200

    Add Int as a variant to the Value type.

--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,6 @@
 /// Represents any valid data type that can be encoded/decoded to and from bencode.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Value {}
+pub enum Value {
+    /// A 64-bit signed integer.
+    Int(i64),
+}
